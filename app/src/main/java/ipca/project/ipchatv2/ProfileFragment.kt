3bafea562477fc61dd5e1d2ae260e88968399bc7
Gabriel Fernandes <fernandes.gaby.tinoco@gmail.com>
2022-01-17 14:34:25 +0000

    erro nas settings

--- app/src/main/java/ipca/project/ipchatv2/ProfileFragment.kt
+++ app/src/main/java/ipca/project/ipchatv2/ProfileFragment.kt
@@ -1,35 +1,25 @@
 package ipca.project.ipchatv2
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.DialogInterface
 import android.content.Intent
-import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
-import ipca.project.ipchatv2.Authentication.LoginActivity
 import ipca.project.ipchatv2.Models.User
 import ipca.project.ipchatv2.databinding.FragmentProfileBinding
-import java.util.*
+
 
 class ProfileFragment : Fragment(R.layout.fragment_profile)  {
 
     //Variables
     lateinit var binding: FragmentProfileBinding
-    lateinit var settings: ImageButton
     lateinit var circleImageView: CircleImageView
     lateinit var buttonEditProfile: Button
     lateinit var username : TextView
@@ -38,7 +28,7 @@ class ProfileFragment : Fragment(R.layout.fragment_profile)  {
     lateinit var email : TextView
     lateinit var studentNumber: TextView
     lateinit var biography: TextView
-    lateinit var logout: ImageButton
+    lateinit var settings: ImageButton
     private lateinit var mAuth: FirebaseAuth
 
     override fun onCreateView(
@@ -61,7 +51,7 @@ class ProfileFragment : Fragment(R.layout.fragment_profile)  {
 
         getCurrentUser()
 
-        settings.setOnClickListener {
+        settings.setOnClickListener() {
             val intent = Intent(activity, SettingsActivity::class.java)
             startActivity(intent)
         }
